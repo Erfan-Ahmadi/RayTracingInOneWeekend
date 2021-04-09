1d7317ce43ec1fa3041069250c85ad4d9ca58fcf
Erfan Ahmadi <ahmadierfan99@gmail.com>
2021-04-09 13:17:05 +0430

    ray class and simple gradient

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,32 @@
 
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+color ray_color(ray const & r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
-    // image info
-
-    int image_width = 1920;
-    int image_height = 1080;
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
     int image_channels = 3;
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     // image data
 
@@ -26,8 +44,14 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color col(double(i) / (image_width-1), double(j) / (image_height-1), 0.75);
-            write_color(pixels, i, j, image_width, image_height, col);
+
+            double u = double(i) / (image_width-1);
+            double v = double(j) / (image_height-1);
+
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+
+            write_color(pixels, i, j, image_width, image_height, pixel_color);
         }
     }
 
