e810cb4ad0b621d79fe77a8ea983e51be4dfa705
Erfan Ahmadi <ahmadierfan99@gmail.com>
2021-05-13 13:47:48 +0430

    instances

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,15 @@ hittable_list cornell_box() {
     objects.add(std::make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(std::make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 
-    objects.add(std::make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
-    objects.add(std::make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
+    std::shared_ptr<hittable> box1 = std::make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = std::make_shared<rotate_y>(box1, 15);
+    box1 = std::make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    std::shared_ptr<hittable> box2 = std::make_shared<box>(point3(0,0,0), point3(165,165,165), white);
+    box2 = std::make_shared<rotate_y>(box2, -18);
+    box2 = std::make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
 
     return objects;
 }
